13157c50f7f439848836435e2cbdb980fc307512
klyed <kylejrturnbull@gmail.com>
2021-12-11 21:01:03 -0600

    switchup loader thiny

--- src/components/_global/BalLoadingIcon/BalLoadingIcon.vue
+++ src/components/_global/BalLoadingIcon/BalLoadingIcon.vue
@@ -1,10 +1,12 @@
-<template>
+<!--<template>
   <div :class="['spinner', sizeClasses]">
     <div :class="`double-bounce1`" />
     <div :class="`double-bounce2`" />
   </div>
+</template>-->
+<templat>
+  <div class="lds-ripple"><div></div><div></div></div>
 </template>
-
 <script>
 import { defineComponent, computed } from 'vue';
 
@@ -120,4 +122,38 @@ export default defineComponent({
     -webkit-transform: scale(1);
   }
 }
+
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+
 </style>

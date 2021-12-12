a9c70613bf3bd5c13440f072219881f0627b5faa
klyed <kylejrturnbull@gmail.com>
2021-12-11 21:46:26 -0600

    fix ripples

--- src/components/_global/BalLoadingIcon/BalLoadingIcon.vue
+++ src/components/_global/BalLoadingIcon/BalLoadingIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="lds-ripple"><div></div><div></div></div>
+  <div :class="ripple"><div></div><div></div></div>
 </template>
 <script>
 import { defineComponent, computed } from 'vue';
@@ -117,23 +117,23 @@ export default defineComponent({
   }
 }*/
 
-.lds-ripple {
+.ripple {
   display: inline-block;
   position: relative;
   width: 80px;
   height: 80px;
 }
-.lds-ripple div {
+.ripple div {
   position: absolute;
   border: 4px solid #fff;
   opacity: 1;
   border-radius: 50%;
-  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+  animation: ripples 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
 }
-.lds-ripple div:nth-child(2) {
+.ripple div:nth-child(2) {
   animation-delay: -0.5s;
 }
-@keyframes lds-ripple {
+@keyframes ripples {
   0% {
     top: 36px;
     left: 36px;
